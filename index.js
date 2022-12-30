5b03b2f7ddca7c8ee6650d2143777e829dc27c01
tsensei <tjsiam019@gmail.com>
2022-12-30 12:22:42 +0600

    Fix CORS and PORT

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
+const cors = require("cors");
 
+app.use(cors());
 app.use(express.json());
 
 morgan.token("body", (req) => {
@@ -104,7 +106,7 @@ app.get("/info", (req, res) => {
   );
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`[+] Server listening on port ${PORT}`);
